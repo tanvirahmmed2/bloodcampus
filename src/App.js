430f8c36993b317b26fa436841fd5477dbaa1511
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-11 14:46:46 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import Footer from "./Component/Footer";
 import Navbar from "./Component/Navbar";
+import Home from './Home'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar/>
-        
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+
+        </Routes>
 
       <Footer/>
       
