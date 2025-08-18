eaf0646daa12ef286b02039c1f4a2b24f4f08fc5
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-19 00:02:20 +0600

    update

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Register from "../users/Register"
 import UsePageTitle from "../Component/UsePageTitle"
+import Intro from '../pages/Intro'
 
 const Home = () => {
   UsePageTitle("Home")
   return (
     <section>
+      <Intro/>
 
       <Register/>
     </section>
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-auto p-2 flex flex-col items-center justify-center'>
+      <div className='w-full mx-40 flex flex-col lg:flex-row items-center justify-center'>
+        <div className='bg-white/5 w-full'>
+
+
+          <p className=' px-4 rounded-lg bg-white/20 w-60'>Community powered • 24/7</p>
+
+
+        </div>
+
+        <div className='w-full '>
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
