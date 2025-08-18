b56c4963446655369ff672375430b49d231de674
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-19 00:07:16 +0600

    update

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Intro = () => {
   return (
     <section className='w-full h-auto p-2 flex flex-col items-center justify-center'>
-      <div className='w-full mx-40 flex flex-col lg:flex-row items-center justify-center'>
-        <div className='bg-white/5 w-full'>
+      <div className='w-4/5 my-12 p-4 rounded-lg bg-white/30 flex flex-col lg:flex-row items-center justify-center'>
+        <div className='bg-black/5 w-full rounded-lg flex flex-col items-start p-4'>
 
 
           <p className=' px-4 rounded-lg bg-white/20 w-60'>Community powered • 24/7</p>
