c31c4906a31c1d702e912b94bffaa633067d17a6
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-18 23:03:26 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 import Home from "./layout/Home";
+import Login from "./users/Login"
+import Register from "./users/Register";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
 
       <Footer />
--- src/users/Login.jsx
+++ src/users/Login.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-const login = () => {
+const Login = () => {
   return (
-    <section>
-      
+    <section className="w-full text-center min-h-screen px-4 md:px-20 flex items-center justify-center">
+      <div className='w-full bg-white/20 rounded-lg flex flex-col md:flex-row items-center justify-center p-4 gap-4'>
+
+        <div className='w-full flex flex-col items-center justify-center text-lg'>
+          <p>Donate Blood, Save Life</p>
+          <p>welcome to</p>
+          <h1 className='text-4xl font-bold'>Blood Campus</h1>
+          <p>a huminty organization to save life</p>
+          <p>Keep donating blood and save people</p>
+
+        </div>
+
+        <form action="" className='w-full flex flex-col items-center justify-center'>
+          <div className='w-full flex flex-col p-2 gap-2 items-start'>
+            <label htmlFor="email">email</label>
+            <input type="email" name='email' id='name' required className='bg-red-500 bg-opacity-15 rounded-lg px-2 p-1 w-full outline-none' />
+          </div>
+          <div className='w-full flex flex-col p-2 gap-2 items-start'>
+            <label htmlFor="password">password</label>
+            <input type="password" name='password' id='password' required className='bg-red-500 bg-opacity-15 rounded-lg px-2 p-1 w-full outline-none' />
+          </div>
+          <button type='submit' className='bg-green-500 p-1 px-3 rounded-lg'>Login</button>
+        </form>
+
+      </div>
+
     </section>
   )
 }
 
-export default login
+export default Login
