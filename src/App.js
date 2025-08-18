4420e64a02fd8910421255b707761d625beca651
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-18 21:55:03 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-import Footer from "./Component/Footer";
-import Navbar from "./Component/Navbar";
-import Home from './Home'
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import Home from "./layout/Home";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar/>
-        <Routes>
-        <Route path="/" element={<Home/>}/>
-
-        </Routes>
-
-      <Footer/>
+      <Navbar />
       
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
