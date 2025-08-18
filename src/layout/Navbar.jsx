a109c1b3572c1121961ff26f201b97dc9f10f76e
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-18 22:18:05 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,14 +3,14 @@ import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <section className='w-full h-auto px-4 bg-white/20 shadow-lg'>
+    <section className='w-full h-auto px-4 bg-white/20 shadow-md'>
       <nav className='w-full h-14 flex flex-row items-center justify-around'>
-        <a href="/" className='font-bold text-3xl'>Blood Campus</a>
-        <div>
-          <Link className='h-14 px-4' to="/">Home</Link>
-          <Link className='h-14 px-4' to="/donor">Donor</Link>
-          <Link className='h-14 px-4' to="/help">Help</Link>
-          <Link className='h-14 px-4' to="/login">LogIn</Link>
+        <a href="/" className='font-bold text-2xl'>Blood Campus</a>
+        <div className='w-auto h-14 flex flex-row items-center justify-center gap-2'>
+          <Link className='h-14 px-4 flex items-center justify-center font-semibold hover:border-b-2' to="/">Home</Link>
+          <Link className='h-14 px-4 flex items-center justify-center font-semibold hover:border-b-2' to="/donor">Donor</Link>
+          <Link className='h-14 px-4 flex items-center justify-center font-semibold hover:border-b-2' to="/help">Help</Link>
+          <Link className='h-10 px-4 flex items-center justify-center font-semibold bg-green-500 rounded-lg font-bold' to="/login">LogIn</Link>
         </div>
       </nav>
     </section>
