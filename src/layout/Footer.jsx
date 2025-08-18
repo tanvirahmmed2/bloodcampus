46c6e3faf1c3313a27528478de147e91bf896045
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-18 22:41:01 +0600

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Footer = () => {
+
+  const [year, setYear] = useState(new Date().getFullYear())
+  useEffect(()=>{
+    setYear(new Date().getFullYear())
+  },[])
+
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-auto p-2 overflow-x-hidden'>
+      <footer className='w-full h-auto text-xs p-4 bg-white/20 rounded-lg flex  flex-row items-center justify-around'>
+        <div className='w-full flex flex-col items-start justify-center gap-2'>
+          <h1 className='text-xl font-bold'>Blood Campus</h1>
+          <p>A humanity organization</p>
+        </div>
+        <div className='w-full flex flex-col items-end justify-center gap-2'>
+          <p>Alrights are reserved {year}</p>
+          <p>Developed by <a href="https://tanvirahmmed.netlify.app" className='text-xl font-bold'>Tanvir Ahmmed</a></p>
+        </div>
+      </footer>
+    </section>
   )
 }
 
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <section className='w-full h-auto px-4 bg-white/20 shadow-md'>
-      <nav className='w-full h-14 flex flex-row items-center justify-around'>
+    <section className='w-full h-auto p-2'>
+      <nav className='w-full h-14 bg-white/20 flex rounded-lg flex-row items-center justify-around'>
         <a href="/" className='font-bold text-2xl'>Blood Campus</a>
         <div className='w-auto h-14 flex flex-row items-center justify-center gap-2'>
           <Link className='h-14 px-4 flex items-center justify-center font-semibold hover:border-b-2' to="/">Home</Link>
