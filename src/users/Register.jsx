1d5e77b12f83e79188799a3ecceee4bd09b5f779
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-10 11:39:54 +0600

    update

--- src/users/Register.jsx
+++ src/users/Register.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import UsePageTitle from "../Component/UsePageTitle";
 import { useContext } from "react";
-import { ThemeContext} from "../Component/ThemeProvider";
+import { ThemeContext } from "../Component/ThemeProvider";
 
 const Register = () => {
   UsePageTitle("Register Donor");
-  const {bloodgroups, districts }= useContext(ThemeContext)
+  const { bloodgroups, districts } = useContext(ThemeContext)
 
   const [formData, setFormData] = useState({
     name: "",
@@ -32,7 +32,7 @@ const Register = () => {
     console.log("Registered Donor:", formData);
   };
 
-  
+
 
   return (
     <section className="w-full min-h-screen p-4 flex items-center justify-center  ">
@@ -41,40 +41,33 @@ const Register = () => {
         <p className=" mb-4">Donate blood, save a life</p>
 
         <form className="w-full text-black flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Full Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-          />
-
-          <input
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-          />
-
-          <div className="flex gap-2">
-            <select
-              name="bloodgroup"
-              value={formData.bloodgroup}
+          <div>
+            <label htmlFor="name">name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              value={formData.name}
               onChange={handleChange}
               required
-              className="w-1/2 px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-            >
-              <option value="">Select Blood Group</option>
-              {bloodgroups.map((bg) => (
-                <option key={bg} value={bg}>{bg}</option>
-              ))}
-            </select>
+              className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+            />
+          </div>
 
+          <div>
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+            />
+          </div>
+          <div>
+            <label htmlFor="dateofbirth">date of birth</label>
             <input
               type="date"
               name="dateofbirth"
@@ -85,49 +78,82 @@ const Register = () => {
             />
           </div>
 
-          <input
-            type="text"
-            name="phone"
-            placeholder="Phone (+880)"
-            value={formData.phone}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-          />
 
-          <select
-            name="district"
-            value={formData.district}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-          >
-            <option value="">Select District</option>
-            {districts.map((d) => (
-              <option key={d} value={d}>{d}</option>
-            ))}
-          </select>
+          <div className="flex gap-2">
+            <div>
+              <label htmlFor="bloodgroup">blood group</label>
+              <select
+                name="bloodgroup"
+                value={formData.bloodgroup}
+                onChange={handleChange}
+                required
+                className="w-1/2 px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+              >
+                <option value="">Select Blood Group</option>
+                {bloodgroups.map((bg) => (
+                  <option key={bg} value={bg}>{bg}</option>
+                ))}
+              </select>
+            </div>
 
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
-          />
+            <div>
+              <label htmlFor="lastdoneted">last doneted</label>
+              <input
+                type="date"
+                name="lastdoneted"
+                value={formData.dateofbirth}
+                onChange={handleChange}
+                required
+                className="w-1/2 px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+              />
+            </div>
 
-          <label className="flex items-center gap-2">
+          </div>
+
+          <div>
+            <label htmlFor="phone">phone</label>
             <input
-              type="checkbox"
-              name="is_available"
-              checked={formData.is_available}
+              type="text"
+              name="phone"
+              placeholder="Phone (+880)"
+              value={formData.phone}
               onChange={handleChange}
-              className="w-4 h-4 accent-red-500"
+              required
+              className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
             />
-            Available for Blood Donation
-          </label>
+          </div>
+
+          <div>
+            <label htmlFor="district">home district</label>
+            <select
+              name="district"
+              value={formData.district}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+            >
+              <option value="">Select District</option>
+              {districts.map((d) => (
+                <option key={d} value={d}>{d}</option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-red-400"
+            />
+          </div>
+
+
+
 
           <button
             type="submit"
